a9805c1c3c8906f2161a196fe569719d805dd3b2
Марк <lolka-pop@rambler.ru>
2023-07-13 11:25:12 +0300

    feat: create build version

--- build/js/free.js
+++ build/js/free.js
@@ -1,154 +1,75 @@
-<<<<<<< Updated upstream
-'use strict';
-
-const freeSlider = new Swiper('.free__slider', {
-=======
 const freeSlider = new Swiper('.free__swiper', {
->>>>>>> Stashed changes
-	speed: 500,
-	wrapperClass: 'free__swiper-wrapper',
-	slideClass: 'free__slide',
-	breakpoints: {
-		769: {
-			slidesPerView: 4,
-			slidesPerGroup: 4,
-			spaceBetween: rem(6),
-		},
-		320: {
-			slidesPerView: 1,
-			spaceBetween: rem(0),
-		},
-    pagination: {
-      clickable: true,
-      renderBullet: function (index, className) {
-        return `
-          <span class="${className}">
-           
-          </span>
-        `;
-      },
-      el: '.popular-fonts__bullets',
+  speed: 500,
+  wrapperClass: 'free__swiper-wrapper',
+  slideClass: 'free__slide',
+  breakpoints: {
+    769: {
+      slidesPerView: 4,
+      spaceBetween: rem(6),
     },
-	},
+    320: {
+      slidesPerView: 1,
+    },
+  },
+  pagination: {
+    clickable: true,
+    renderBullet: function (index, className) {
+      return `
+        <span class="${className}">
+         
+        </span>
+      `;
+    },
+    el: '.popular-fonts__bullets',
+  },
 });
 
-<<<<<<< Updated upstream
 // слайдер рекомендаций
 
-const recomSlider = new Swiper('.free__recom-slider', {
-//   speed: 500,
-//   loop: true,
-//   slidesPerView: 'auto',
-//   centeredSlides: true,
-//   spaceBetween: rem(0),
-//   breakpoints: {
-//     769: {
-//       slidesPerView: 6,
-//     },
-//     320: {
-//       slidesPerView: 2,
-//     },
-//   },
 
-loop: true,
-wrapperClass: 'free__recom-wrapper',
-slideClass: 'free__recom-slider-item',
-slideActiveClass: 'active',
-slidesPerView: 'auto',
-watchOverflow: true,
-autoHeight: true,
-centeredSlides: true,
-
-
-
-
-
- 
+const zeroFreeSwiper = new Swiper('.free__recom-zero-slider', {
+	slidesPerView: 'auto',
+	initialSlide: 1,
+	loop: true,
 });
-
-
-
-
-
-const swiper1 = new Swiper(".testing__swiper", {
+const secondFreeSwiper = new Swiper('.free__recom-second-slider', {
 	slidesPerView: 'auto',
 	loop: true,
-	
+	initialSlide: 2,
 
-});
-const swiper2 = new Swiper(".testing__swiper-two", {
-	slidesPerView: 'auto',
-	loop: true,
-	
 	effect: 'fade',
 	fadeEffect: {
-		crossFade: true
+		crossFade: true,
 	},
-	
+	navigation: {
+    prevEl: '.general__prev',
+    nextEl: '.general__next',
+  },
+  pagination: {
+    el: '.general__fraction',
+    type: 'fraction',
+    formatFractionCurrent: addZero,
+    formatFractionTotal: addZero,
+  },
 });
-const swiper3 = new Swiper(".testing__swiper-three", {
+const firstFreeSwiper = new Swiper('.free__recom-first-slider', {
 	loop: true,
 	slidesPerView: 'auto',
-	
-	
+	initialSlide: 3
 });
 
 const swipeAllSliders = (index) => {
-  swiper1.slideTo(index);
-  swiper2.slideTo(index);
-  swiper3.slideTo(index);
-}
+	zeroFreeSwiper.slideTo(index);
+	firstFreeSwiper.slideTo(index);
+	secondFreeSwiper.slideTo(index);
+};
 
-swiper1.on('slideChange', () => swipeAllSliders(swiper1.activeIndex));
-swiper2.on('slideChange', () => swipeAllSliders(swiper2.activeIndex));
-swiper3.on('slideChange', () => swipeAllSliders(swiper3.activeIndex));
-=======
-//слайдер рекомендаций
-const recomZeroSlider = new Swiper('.free__recom-zero-slider', {
-	speed: 500,
-	loop: true,
-	spaceBetween: rem(0),
-	breakpoints: {
-		769: {
-			slidesPerView: 2,
-		},
-		320: {
-			slidesPerView: 2,
-		},
-	},
-});
-
-const recomFirstSlider = new Swiper('.free__recom-first-slider', {
-	speed: 500,
-	loop: true,
-	spaceBetween: rem(0),
-	breakpoints: {
-		769: {
-			slidesPerView: 2,
-		},
-		320: {
-			slidesPerView: 2,
-		},
-	},
-});
-
-const recomSecondSlider = new Swiper('.free__recom-second-slider', {
-	speed: 500,
-	loop: true,
-	centeredSlides: true,
-	spaceBetween: rem(0),
-	effect: 'fade',
-	breakpoints: {
-		769: {
-			slidesPerView: 1,
-		},
-		320: {
-			slidesPerView: 2,
-		},
-	},
-	navigation: {
-		prevEl: '.general__prev',
-		nextEl: '.general__next',
-	},
-});
->>>>>>> Stashed changes
+firstFreeSwiper.on('slideChange', () =>
+	swipeAllSliders(firstFreeSwiper.activeIndex),
+);
+zeroFreeSwiper.on('slideChange', () =>
+	swipeAllSliders(zeroFreeSwiper.activeIndex),
+);
+secondFreeSwiper.on('slideChange', () =>
+	swipeAllSliders(secondFreeSwiper.activeIndex),
+);
