d72289dd6fdfa8a743ec0bfed321009ba17b524e
Марк <lolka-pop@rambler.ru>
2023-08-02 14:56:59 +0300

    feat: add Dmitry's files and produce some fixes

--- build/js/charts-script.js
+++ build/js/charts-script.js
@@ -1,6 +1,36 @@
-"use strict";
+'use strict';
 
+const ctxChart = document.getElementById('myChart');
+if (ctxChart) {
+	const myChartLine = new Chart(ctxChart.getContext('2d'), {
+		type: 'line',
+		data: {
+			labels: [
+				'03.22',
+				'04.22',
+				'05.22',
+				'06.22',
+				'07.22',
+				'08.22',
+				'09.22',
+				'10.22',
+				'11.22',
+			],
+			datasets: [
+				{
+					data: [10000, 20000, 30000, 80000, 30000, 20000, '', '', '', ''],
+					label: 'Pending',
+					borderWidth: 1,
+					borderColor: 'rgba(255, 180, 47, 1)',
+					backgroundColor: 'rgba(222, 117, 94, 0.2)',
+					fill: true,
+				},
+			],
+		},
+	});
+}
 
+<<<<<<< HEAD
 const ctxChart = document.getElementById('myChart').getContext('2d');
 const myChartLine = new Chart(ctxChart, {
 	type: 'line',
@@ -98,64 +128,93 @@ const myChartLine = new Chart(ctxChart, {
         }
     },
 });
+=======
+const ctxChartTwo = document.getElementById('myChartTwo');
+if (ctxChartTwo) {
+	const myChartLineTwo = new Chart(ctxChartTwo.getContext('2d'), {
+		type: 'line',
+		data: {
+			labels: [
+				'03.22',
+				'04.22',
+				'05.22',
+				'06.22',
+				'07.22',
+				'08.22',
+				'09.22',
+				'10.22',
+				'11.22',
+			],
+			datasets: [
+				{
+					data: [10000, 20000, 30000, 80000, 30000, 20000, '', '', '', ''],
+					label: 'Pending',
+					borderWidth: 1,
+					borderColor: 'rgba(255, 180, 47, 1)',
+					backgroundColor: 'rgba(222, 117, 94, 0.2)',
+					fill: true,
+				},
+			],
+		},
+	});
+}
+>>>>>>> a1fd0222 (feat: add Dmitry's files and produce some fixes)
 
-
-const ctxChartTwo = document.getElementById('myChartTwo').getContext('2d');
-const myChartLineTwo = new Chart(ctxChartTwo, {
-	type: 'line',
-	data: {
-	  labels: ["03.22", "04.22", "05.22", "06.22", "07.22", "08.22", "09.22", "10.22", "11.22"],
-	  datasets: [{ 
-		 
-		 
-		  data: [10000,20000,30000,80000,30000,20000,"","","",""],
-		  label: "Pending",
-		  borderWidth:1,
-		  borderColor: "rgba(255, 180, 47, 1)",
-		  backgroundColor: "rgba(222, 117, 94, 0.2)",
-		  fill: true,
-		}, 
-	  ]
-	},
-});
-
-
-
-const ctxChartThree = document.getElementById('myChartThree').getContext('2d');
-const myChartLineThree = new Chart(ctxChartThree, {
-	type: 'line',
-	data: {
-	  labels: ["03.22", "04.22", "05.22", "06.22", "07.22", "08.22", "09.22", "10.22", "11.22"],
-	  datasets: [{ 
-		 
-		 
-		  data: [10000,20000,30000,80000,30000,20000,"","","",""],
-		  label: "Pending",
-		  borderWidth:1,
-		  borderColor: "rgba(255, 180, 47, 1)",
-		  backgroundColor: "rgba(222, 117, 94, 0.2)",
-		  fill: true,
-		}, 
-	  ]
-	},
-});
-
-
-const ctxChartFour = document.getElementById('myChartFour').getContext('2d');
-const myChartLineFour = new Chart(ctxChartFour, {
-	type: 'line',
-	data: {
-	  labels: ["03.22", "04.22", "05.22", "06.22", "07.22", "08.22", "09.22", "10.22", "11.22"],
-	  datasets: [{ 
-		 
-		 
-		  data: [10000,20000,30000,80000,30000,20000,"","","",""],
-		  label: "Pending",
-		  borderWidth:1,
-		  borderColor: "rgba(255, 180, 47, 1)",
-		  backgroundColor: "rgba(222, 117, 94, 0.2)",
-		  fill: true,
-		}, 
-	  ]
-	},
-});
+const ctxChartThree = document.getElementById('myChartThree');
+if (ctxChartThree) {
+	const myChartLineThree = new Chart(ctxChartThree.getContext('2d'), {
+		type: 'line',
+		data: {
+			labels: [
+				'03.22',
+				'04.22',
+				'05.22',
+				'06.22',
+				'07.22',
+				'08.22',
+				'09.22',
+				'10.22',
+				'11.22',
+			],
+			datasets: [
+				{
+					data: [10000, 20000, 30000, 80000, 30000, 20000, '', '', '', ''],
+					label: 'Pending',
+					borderWidth: 1,
+					borderColor: 'rgba(255, 180, 47, 1)',
+					backgroundColor: 'rgba(222, 117, 94, 0.2)',
+					fill: true,
+				},
+			],
+		},
+	});
+}
+const ctxChartFour = document.getElementById('myChartFour');
+if (ctxChartFour) {
+	const myChartLineFour = new Chart(ctxChartFour.getContext('2d'), {
+		type: 'line',
+		data: {
+			labels: [
+				'03.22',
+				'04.22',
+				'05.22',
+				'06.22',
+				'07.22',
+				'08.22',
+				'09.22',
+				'10.22',
+				'11.22',
+			],
+			datasets: [
+				{
+					data: [10000, 20000, 30000, 80000, 30000, 20000, '', '', '', ''],
+					label: 'Pending',
+					borderWidth: 1,
+					borderColor: 'rgba(255, 180, 47, 1)',
+					backgroundColor: 'rgba(222, 117, 94, 0.2)',
+					fill: true,
+				},
+			],
+		},
+	});
+}
