0365b930f3f1f485b67d021659adfd28df13af5d
Марк <lolka-pop@rambler.ru>
2023-08-03 16:03:42 +0300

    feat: add animation for cart-item

--- build/js/cart.js
+++ build/js/cart.js
@@ -12,13 +12,12 @@ const showItemInfoClass = 'cart__item-info--show';
 const activeModalItemClass = 'modal__item--active';
 
 //обработка удаления элемента при мобильном отображении + обработка 'hover'
-orderItems.forEach((element) => {
-	const moveItemBtn = element.querySelector('.cart__item-mob-close-btn');
-	const item = element.querySelector('.cart__item');
-	const itemInfoIcon = element.querySelector('.cart__item-info-icon');
-	const itemInfo = element.querySelector('.cart__item-info');
+if (orderItems) {
+	orderItems.forEach((element) => {
+		const item = element.querySelector('.cart__item');
+		const itemInfoIcon = element.querySelector('.cart__item-info-icon');
+		const itemInfo = element.querySelector('.cart__item-info');
 
-	if (moveItemBtn) {
 		item.addEventListener('click', (e) => {
 			if (
 				!e.target.classList.contains('cart__item-license') &&
@@ -31,35 +30,31 @@ orderItems.forEach((element) => {
 			}
 		});
 
-		moveItemBtn.addEventListener('click', () => {
-			item.classList.remove(moveItemClass);
-		});
-	}
-
-	if (itemInfoIcon) {
-		itemInfoIcon.addEventListener('mouseover', () => {
-			if (window.innerWidth > 768) {
-				itemInfo.classList.add(showItemInfoClass);
-			}
-		});
-		itemInfoIcon.addEventListener('mouseout', () => {
-			if (window.innerWidth > 768) {
-				itemInfo.classList.remove(showItemInfoClass);
-			}
-		});
-
-		itemInfoIcon.addEventListener('touchstart', () => {
-			if (window.innerWidth < 769) {
-				console.log(window.innerWidth);
-				if (item.classList.contains(moveItemClass)) {
-					item.classList.remove(moveItemClass);
-					return;
+		if (itemInfoIcon) {
+			itemInfoIcon.addEventListener('mouseover', () => {
+				if (window.innerWidth > 768) {
+					itemInfo.classList.add(showItemInfoClass);
 				}
-				itemInfo.classList.toggle(showItemInfoClass);
-			}
-		});
-	}
-});
+			});
+
+			itemInfoIcon.addEventListener('mouseout', () => {
+				if (window.innerWidth > 768) {
+					itemInfo.classList.remove(showItemInfoClass);
+				}
+			});
+
+			itemInfoIcon.addEventListener('touchstart', () => {
+				if (window.innerWidth < 769) {
+					if (item.classList.contains(moveItemClass)) {
+						item.classList.remove(moveItemClass);
+						return;
+					}
+					itemInfo.classList.toggle(showItemInfoClass);
+				}
+			});
+		}
+	});
+}
 
 //обработка показа модального окошка с выбором лицензии
 if (cartItems) {
@@ -68,10 +63,47 @@ if (cartItems) {
 		const licensePopup = item.querySelector('.cart__item-popup');
 		const licenseBtn = item.querySelector('.cart__item-license');
 
-		console.log(licensePopup, licenseBtn);
-
 		licenseBtn.addEventListener('click', () => {
 			licensePopup.classList.toggle(popupShowClass);
 		});
+	});
+}
+
+const activeAnimationClass = 'active';
+
+if (orderItems) {
+	orderItems.forEach((element) => {
+		const handleMobileSwipe = () => {
+			let touchStartX = 0;
+			let touchEndX = 0;
+
+			element.addEventListener('touchstart', (event) => {
+				touchStartX = event.touches[0].clientX;
+			});
+
+			element.addEventListener('touchmove', (event) => {
+				touchStartX = event.touches[0].clientX;
+			});
+
+			element.addEventListener('touchend', (event) => {
+				handleSwipe(element, event);
+			});
+
+			function handleSwipe(elem, event) {
+				const swipeDistance = touchEndX - touchStartX;
+				const item = elem.querySelector('.cart__item');
+				if (swipeDistance < 30) {
+					if (
+						!event.target.classList.contains('cart__item-info-icon') &&
+						!event.target.classList.contains('cart__item-license')
+					) {
+						item.classList.toggle(moveItemClass);
+						item.classList.toggle(activeAnimationClass);
+					}
+				}
+			}
+		};
+
+		handleMobileSwipe();
 	});
 }
